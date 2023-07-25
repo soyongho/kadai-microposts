87c528de7a237202a2b2203bd20f854e1c2eadc2
yonho.so <yongho_120@yahoo.co.jp>
2023-07-25 15:00:42 +0000

    likes/fix

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,16 +2,16 @@ class FavoritesController < ApplicationController
   before_action :require_user_logged_in
   
   def create
-    user = User.find(params[:micropost_id])
-    current_user.like(user)
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.like(micropost)
     flash[:success] = 'お気に入り登録しました。'
-    redirect_to user
+    redirect_to request.referer
   end
 
   def destroy
-    user = User.find(params[:micropost_id])
-    current_user.dislike(user)
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.dislike(micropost)
     flash[:success] = 'お気に入り登録を解除しました。'
-    redirect_to user
+    redirect_to request.referer
   end
 end
